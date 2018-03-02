a9c7e3fad00853913d2e978c6ce790f672b47910
katlyncoppage <katlyncoppage@gmail.com>
2018-03-01 19:25:22 -0600

    Finished the project!!!

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models; //added
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -18,9 +19,8 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job someJob = jobData.Find(id);
+            return View(someJob);
         }
 
         public IActionResult New()
@@ -32,9 +32,34 @@ namespace TechJobs.Controllers
         [HttpPost]
         public IActionResult New(NewJobViewModel newJobViewModel)
         {
-            // TODO #6 - Validate the ViewModel and if valid, create a 
+            //TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
-            // redirect to the Job detail (Index) action/view for the new Job.
+            // redirect to the Job detail(Index) action / view for the new Job.
+
+             if (ModelState.IsValid)
+                {
+
+                Employer newEmployer = jobData.Employers.Find(newJobViewModel.EmployerID);
+                Location newLocation = jobData.Locations.Find(newJobViewModel.LocationID);
+                CoreCompetency newCoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID);
+                PositionType newPositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID);
+
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Location = newLocation,
+                    Employer = newEmployer,
+                    CoreCompetency = newCoreCompetency,
+                    PositionType = newPositionType
+                };
+
+                jobData.Jobs.Add(newJob);
+
+
+
+                    return Redirect(string.Format("/Job?ID={0}", newJob.ID));
+                }
+
 
             return View(newJobViewModel);
         }
